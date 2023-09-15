70fd977842accce1ecd4f654de51913c01b131dc
s1ks1 <sinisaz123@gmail.com>
2023-09-15 11:34:11 +0200

    Add the index controller

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    #[Route('/', name: 'app_conference')]
+    public function index(): Response
+    {
+        return new Response(<<<EOF
+<html lang="en">
+    <body>
+        <h1>Conference</h1>
+        <img class="image" src="/images/conference.jpg" alt="Conference" width="600" height="400"/>"
+    </body>
+</html>
+EOF
+);
+    }
+}
